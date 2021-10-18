b8bf02f35cc590a7bc8513b10b4989235f1afc3f
NehaSamant024 <91238040+NehaSamant024@users.noreply.github.com>
2021-10-18 22:42:32 +0530

    Add files via upload

---
+++ practical 7/prac07_task7.5.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using  namespace std;
+int main()
+{ int arr[4][4];
+  int n;
+  cout<<"Enter the value of n\n";
+  cin>>n;
+  for(int i=0;i<n;i++)
+  {
+   cout<<int j=i;j<=i;j++)
+   {
+     cout<<"*"<<endl;
+     }
+     }
+     return 0;
+    } 
